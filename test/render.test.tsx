3e6b4e55124c280ce079d7402ea7ced4585f960f
Ben Durrant <ben.durrant@marketdojo.com>
2025-09-12 14:51:47 +0100

    use bound click method

--- test/render.test.tsx
+++ test/render.test.tsx
@@ -1,6 +1,6 @@
 import { html } from 'lit'
 import { expect, test } from 'vitest'
-import { page, userEvent } from '@vitest/browser/context'
+import { page } from '@vitest/browser/context'
 import { render } from '../src/index'
 import './fixtures/hello-world'
 import './fixtures/counter'
@@ -14,6 +14,6 @@ test('renders counter', async () => {
   const screen = render(html`<counter-element count="1"></counter-element>`)
 
   await expect.element(screen.getByText('Count is 1')).toBeVisible()
-  await userEvent.click(screen.getByRole('button', { name: 'Increment' }))
+  await screen.getByRole('button', { name: 'Increment' }).click()
   await expect.element(screen.getByText('Count is 2')).toBeVisible()
 })
